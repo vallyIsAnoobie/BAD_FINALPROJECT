6628a75a5b888642f3abb32c5dde75c0a841947a
vallyIsAnoobie <valencia.hartono101@gmail.com>
2024-12-21 13:31:47 +0700

    Create register.blade.php
    commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="globals.css" />
+    <link rel="stylesheet" href="styleguide.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="REGISTER-PAGE">
+      <div class="overlap-wrapper">
+        <div class="overlap">
+          <div class="cover-backgrounds">
+            <img class="vector" src="img/vector-3.svg" /> <img class="img" src="img/vector-2.svg" />
+          </div>
+          <div class="group">
+            <div class="overlap-group">
+              <div class="text-wrapper">Maison Burro</div>
+              <div class="div">バターケーキ</div>
+            </div>
+          </div>
+          <div class="overlap-group-wrapper">
+            <div class="overlap-2">
+              <img class="line" src="img/line.svg" />
+              <div class="div-wrapper">
+                <div class="overlap-3">
+                  <img class="bouquet-wrapper" src="img/under.svg" />
+                  <div class="flowers">
+                    <div class="overlap-group-2">
+                      <img class="daisy-stems" src="img/daisy-stems.svg" />
+                      <img class="daisy" src="img/daisy-3.svg" />
+                      <img class="daisy-2" src="img/daisy-2.svg" />
+                      <img class="daisy-3" src="img/daisy-5.svg" />
+                      <img class="daisy-4" src="img/daisy-5.svg" />
+                      <img class="daisy-5" src="img/daisy-5.svg" />
+                      <img class="daisy-6" src="img/daisy.svg" />
+                      <img class="daisy-7" src="img/daisy-5.svg" />
+                      <img class="daisy-8" src="img/daisy-5.svg" />
+                      <img class="daisy-9" src="img/daisy-5.svg" />
+                      <img class="daisy-10" src="img/daisy-4.svg" />
+                      <img class="daisy-11" src="img/daisy-2.svg" />
+                      <img class="daisy-12" src="img/daisy-4.svg" />
+                      <img class="daisy-13" src="img/daisy-3.svg" />
+                      <img class="daisy-14" src="img/daisy-3.svg" />
+                      <img class="daisy-15" src="img/daisy-3.svg" />
+                      <img class="daisy-16" src="img/daisy-6.svg" />
+                    </div>
+                  </div>
+                  <div class="flowers-2">
+                    <div class="overlap-4">
+                      <img class="tulip-stems" src="img/tulip-stems.svg" />
+                      <img class="tulip" src="img/tulip-2.svg" />
+                      <img class="tulip-2" src="img/tulip-3.svg" />
+                      <img class="tulip-3" src="img/tulip-4.svg" />
+                      <img class="tulip-4" src="img/tulip.svg" />
+                      <img class="tulip-5" src="img/image.svg" />
+                    </div>
+                  </div>
+                  <img class="bouquet-wrapper-over" src="img/over.svg" />
+                  <img class="bouquet-ties" src="img/bouquet-ties.svg" />
+                </div>
+              </div>
+              <img class="icons" src="img/icons.svg" />
+              <img class="fuzzy-friends" src="img/fuzzy-friends-curious-bunny.png" />
+            </div>
+          </div>
+          <div class="group-2">
+            <div class="overlap-5">
+              <img class="rectangle" src="img/rectangle-6412.svg" />
+              <img class="rectangle-2" src="img/rectangle-6416.svg" />
+              <div class="group-3">
+                <div class="ellipse"></div>
+                <div class="ellipse-2"></div>
+              </div>
+              <div class="group-4">
+                <div class="ellipse"></div>
+                <div class="ellipse-2"></div>
+              </div>
+              <p class="don-t-loaf-around">
+                <span class="span">&#34;Don’t loaf around—<br /></span>
+                <span class="text-wrapper-2">register</span>
+                <span class="span"> today and rise with us!”</span>
+              </p>
+              <div class="group-5">
+                <div class="text-wrapper-3">First Name</div>
+                <div class="rectangle-3"></div>
+              </div>
+              <div class="group-6">
+                <div class="text-wrapper-4">Last Name</div>
+                <div class="rectangle-3"></div>
+              </div>
+              <div class="group-7">
+                <div class="text-wrapper-3">Email</div>
+                <div class="rectangle-4"></div>
+              </div>
+              <div class="group-8">
+                <div class="text-wrapper-3">Password</div>
+                <div class="rectangle-5"></div>
+              </div>
+              <div class="group-9">
+                <div class="text-wrapper-3">Re-enter password</div>
+                <div class="rectangle-6"></div>
+              </div>
+              <div class="group-10">
+                <div class="overlap-group-3"><div class="text-wrapper-5">Register</div></div>
+              </div>
+            </div>
+          </div>
+          <div class="group-wrapper">
+            <div class="group-11">
+              <div class="overlap-group-4">
+                <img class="vector-2" src="img/vector.svg" />
+                <div class="text-wrapper-6">here</div>
+                <div class="text-wrapper-7">Login</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
