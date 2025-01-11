ea68f58177d50d472660255ccc4f58f9e8172106
valencia <valencia.hartono101@gmail.com>
2025-01-11 20:34:20 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,28 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\AuthController; // Ensure this is imported
 
+// Set the default route to display the splashscreen view
 Route::get('/', function () {
-    return view('register');
+    return view('splashscreen');
 });
 
-// Route::get('/product', function () {
-//     return view('product');
-// });
-
-//Product Routing
+// Product Routing
 Route::get('/products', [ProductController::class, 'showProducts']);
 
-//Order Routing
+// Order Routing
 Route::get('/order-summary/{orderId}', [OrderController::class, 'showOrderSummary']);
 
-//Customer Routing
-Route::get('/register', [CustomerController::class, 'showRegisterForm'])->name('customer.register.form');
-Route::post('/register', [CustomerController::class, 'register'])->name('customer.register');
+// Customer Routing
+Route::get('/login', function () {
+    return view('login'); // Login view
+})->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+
+Route::get('/signup', function () {
+    return view('signup'); // Signup view
+})->name('signup');
+
+Route::post('/signup', [AuthController::class, 'signup'])->name('signup.submit');
